80f6928a8b4e74a9ae99878930245b17fff5194f
Lukasz Gorny <l.gorny@student.uw.edu.pl>
2026-05-04 15:08:21 +0200

    Implement routing_scope
    Added routing_scope representation with URL enconding and unit tests.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod config;
 mod customize;
 mod header_whitelist;
 mod interceptors;
+mod routing_scope;
 
 pub use crate::client::*;
 pub use crate::compression::*;
@@ -11,3 +12,4 @@ pub use crate::config::*;
 pub use crate::customize::*;
 pub(crate) use crate::header_whitelist::*;
 pub(crate) use crate::interceptors::*;
+pub use crate::routing_scope::*;
